3783e143523b5b2e0b3feb3cdd908bc938a8bc0d
chukimmuoi <chukimmuoi@gmail.com>
2024-11-16 14:49:51 +0700

    Remove ComponentActivity --> AppCompatActivity

--- app/src/main/java/com/chukimmuoi/jetchatbyme/MainViewModel.kt
+++ app/src/main/java/com/chukimmuoi/jetchatbyme/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.chukimmuoi.jetchatbyme
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 /**
  * @author: My Project
@@ -11,6 +14,19 @@ import androidx.lifecycle.ViewModel
  * @Project: JetchatByMe
  * Created by chukimmuoi on 16/11/24.
  */
+/**
+ * Được sử dụng để giao tiếp giữa các màn hình.
+ * */
 class MainViewModel : ViewModel() {
 
+    private val _drawerShouldBeOpened = MutableStateFlow(false)
+    var drawerShouldBeOpened = _drawerShouldBeOpened.asStateFlow()
+
+    fun openDrawer() {
+        _drawerShouldBeOpened.update { true }
+    }
+
+    fun resetOpenDrawerAction() {
+        _drawerShouldBeOpened.update { false }
+    }
 }
--- app/src/main/java/com/chukimmuoi/jetchatbyme/NavActivity.kt
+++ app/src/main/java/com/chukimmuoi/jetchatbyme/NavActivity.kt
@@ -1,9 +1,10 @@
 package com.chukimmuoi.jetchatbyme
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,7 +14,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.chukimmuoi.jetchatbyme.ui.theme.JetchatByMeTheme
 
-class NavActivity : ComponentActivity() {
+/**
+ * Dùng AppCompatActivity:
+ *
+ * Khi cần hỗ trợ tương thích ngược cho các tính năng hiện đại.
+ * Khi dự án sử dụng giao diện XML truyền thống với Action Bar hoặc Drawer Menu.
+ *
+ * Dùng ComponentActivity:
+ *
+ * Khi sử dụng Jetpack Compose.
+ * Khi muốn tổ chức logic ứng dụng linh hoạt và tránh cấu trúc kế thừa phức tạp.
+ * */
+class NavActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
